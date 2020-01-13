2f7b0563a9b66f3612fb131b5783e110bc40b4b0
en.navid <en.navid@outlook.com>
2020-01-13 12:38:39 +0330

    Add high and low to forcast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,13 +3,14 @@ const request = require('request')
 forecast = (latitude, longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/8f4bf50aae4b4ec8667979bde3727748/' + latitude + ',' + longitude;
 
-    request({url, json: true }, (error, {body}) => {
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable connect to weather service !', undefined);
         } else if (body.error) {
             callback('Unable to find location', undefined);
         } else {
-            callback(undefined, body.daily.data[0].summary + 'It is currently ' + body.currently.temperature + ' degree out . There is a ' + body.currently.precipProbability)
+
+            callback(undefined, body.daily.data[0].summary + 'It is currently ' + body.currently.temperature + ' degree out .This high today is ' + body.daily.data[0].temperatureHigh + ' with a low of ' + body.daily.data[0].temperatureLow + ' There is a ' + body.currently.precipProbability)
         }
     })
 }
